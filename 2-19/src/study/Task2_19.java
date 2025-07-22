2412a58df772d5747f0570508df4e30012c86b5e
YuricoOyae <bbc.oyae@gmail.com>
2025-07-22 21:41:40 +0900

    2-19

---
+++ 2-19/src/study/Task2_19.java
@@ -0,0 +1,12 @@
+package study;
+import constants.FruitsConstants;
+import fruits.Fruits;
+public class Task2_19 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		 Fruits.printFruits(FruitsConstants.FRUITS_LEMON_04, FruitsConstants.FRUITS_COUNT_20);
+
+	}
+
+}
