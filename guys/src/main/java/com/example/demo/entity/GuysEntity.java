9159dc8cad5db801875425fe69afa67bc2c10cec
YuricoOyae <bbc.oyae@gmail.com>
2025-09-06 23:15:10 +0900

    4-22

---
+++ guys/src/main/java/com/example/demo/entity/GuysEntity.java
@@ -0,0 +1,64 @@
+package com.example.demo.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+/**
+ * ユーザー情報 Entity
+ */
+@Data
+@Entity
+@Table(name = "guys", schema = "public")
+public class GuysEntity  {
+
+  /**
+   * ID
+   */
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Integer id;
+
+
+  /**
+   * 名前
+   */
+  @Column(name = "name")
+  private String name;
+
+
+  /**
+   * 住所
+   */
+  @Column(name = "address")
+  private String address;
+
+
+  /**
+   * 電話番号
+   */
+  @Column(name = "phone")
+  private String phone;
+
+
+  /**
+   * 入社日
+   */
+  @Column(name = "join_date")
+  private Date updateDate;
+
+
+  /**
+   * 昇格日
+   */
+  @Column(name = "promotion_date")
+  private Date promotionDate;
+}
